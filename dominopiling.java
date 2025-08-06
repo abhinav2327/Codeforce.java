e965b3d7ced6cca4466072308d69296a07f4ea37
Abhinav Kumar <abhinavjsr2003@gmail.com>
2025-08-06 19:58:59 +0530

    6-8-25

---
+++ dominopiling.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class dominopiling {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int ans = (n * m) / 2;
+        System.out.println(ans);
+    }
+
+}
